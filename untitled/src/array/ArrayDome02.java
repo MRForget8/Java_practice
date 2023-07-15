f6fea2fb64f9401605334b8da753d72f76158f65
MRForget8 <130636311+MRForget8@users.noreply.github.com>
2023-07-15 19:48:42 +0800

    practice

--- untitled/src/array/ArrayDome02.java
+++ untitled/src/array/ArrayDome02.java
@@ -3,10 +3,16 @@ package array;
 public class ArrayDome02 {
     public static void main(String[] args) {
         //静态初始化 创建+赋值
-        int[] a = {1,2,3,4,5,6,7,8,9};
+        //增强for循环写法
+        //int[] a = {1,2,3,4,5,6,7,8,9};
         //Man[] mans = {new Man()};
-        for (int j : a) {
-            System.out.println(j);
+        //for (int j : a) {
+            //System.out.println(j);
+        //}
+        //普通for循环写法
+        int[] a = {1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
         }
         //溢出
         //System.out.println(a[9]);

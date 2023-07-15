720cc5fd64b6076b2d7c820146b53d7634d098dd
MRForget8 <130636311+MRForget8@users.noreply.github.com>
2023-07-15 21:33:53 +0800

    practice

--- untitled/src/array/ArrayDome04.java
+++ untitled/src/array/ArrayDome04.java
@@ -4,8 +4,21 @@ public class ArrayDome04 {
     public static void main(String[] args) {
         int[] arrays = {1, 2, 3, 4, 5};
 
-        for (int array : arrays){
-            System.out.println(array);
+        printArray(arrays);
+        reverse(arrays);
+    }
+
+    //打印数组元素
+    public static void printArray(int[] arrays) {
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.println(arrays[i]);
+        }
+    }
+
+    //反转数组
+    public static void reverse(int[] arrays) {
+        for (int i = arrays.length - 1; i >= 0; i--) {
+            System.out.println(arrays[i]);
         }
     }
 }

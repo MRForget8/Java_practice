b2851ba67c3f1f1f0813b31540783a6785f70072
MRForget8 <130636311+MRForget8@users.noreply.github.com>
2023-07-15 19:44:57 +0800

    practice

--- untitled/src/array/ArrayDome02.java
+++ untitled/src/array/ArrayDome02.java
@@ -5,13 +5,17 @@ public class ArrayDome02 {
         //静态初始化 创建+赋值
         int[] a = {1,2,3,4,5,6,7,8,9};
         //Man[] mans = {new Man()};
-        System.out.println(a[0]);
+        for (int j : a) {
+            System.out.println(j);
+        }
+        //溢出
+        //System.out.println(a[9]);
 
         //动态初始化
-        int[] b = new int[10];
-        b[0] = 10;
-        for (int i = 0; i < b.length; i++){
-            System.out.println(b[i]);
-        }
+//        int[] b = new int[10];
+//        b[0] = 10;
+//        for (int i = 0; i < b.length; i++){
+//            System.out.println(b[i]);
+//        }
     }
 }
